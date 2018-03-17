fdac35b0f60543fc8c2b115f74a1c62b8426bf16
Marco Antonio Moyses Monteiro Junior <marco.monteiro@fatec.sp.gov.br>
2018-03-17 00:55:20 -0300

    Ajustes das Views de Carrinho e Carrossel

--- E-Conc/E-Conc/Controllers/PedidoController.cs
+++ E-Conc/E-Conc/Controllers/PedidoController.cs
@@ -1,17 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_Conc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace E_Conc.Controllers
 {
     public class PedidoController : Controller
-    {
+    {     
+        List<Produto> produtos = new List<Produto>
+        {
+            new Produto(0, "E-Commerce", "DesenvolvimentoProduto.jpg", "Desenvolvimento"),
+            new Produto(1, "StartUp", "EmpreendedorismoProduto.jpg", "Empreendedorismo"),
+            new Produto(2, "Software BitCoins", "IniciacaoCientificaProduto.jpg", "Iniciação Cientifica"),
+            new Produto(3, "História da Computação", "PesquisaAcademicaProduto.jpg", "Pesquisa Acadêmica")
+        };
+        
+        Orientador orientador = new Orientador("Marco Jr.");
+        Curso curso = new Curso("Análise e Desenvolvimento de Sistemas", "ADS");
+
         public IActionResult Carrossel()
         {
-            return View();
+            return View(produtos);
         }
 
         public IActionResult Carrinho()
         {
-            return View();
+            var itemCarrinho = new ItemPedido(1, produtos[0], orientador, curso);           
+
+            return View(itemCarrinho);
         }
 
         public IActionResult Resumo()
---
+++ E-Conc/E-Conc/Models/Curso.cs
@@ -0,0 +1,14 @@
+﻿namespace E_Conc.Models
+{
+    public class Curso
+    {
+        public string Nome { get; set; }
+        public string Sigla { get; set; }
+
+        public Curso(string nome, string sigla)
+        {
+            Nome = nome;
+            Sigla = sigla;
+        }
+    }
+}
