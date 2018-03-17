4d1792c02fc2041da6917bc601a8b781621c22d8
Marco Antonio Moyses Monteiro Junior <marco.monteiro@fatec.sp.gov.br>
2018-03-17 17:43:25 -0300

    Dinamização da View de Resumo

---
+++ E-Conc/E-Conc/Models/Aluno.cs
@@ -0,0 +1,25 @@
+﻿namespace E_Conc.Models
+{
+    public class Aluno
+    {
+        public int Ra { get; private set; }
+        public string Nome { get; private set; }
+        public string Telefone { get; private set; }
+        public string Instituicao { get; private set; }
+        public string InstituicaoSigla { get; private set; }
+        public Curso Curso { get; private set; }
+        public string Email { get; private set; }
+
+        public Aluno(int ra, string nome, string telefone, string instituicao, string instituicaoSigla, 
+            Curso curso, string email)
+        {
+            Ra = ra;
+            Nome = nome;
+            Telefone = telefone;
+            Instituicao = instituicao;
+            InstituicaoSigla = instituicaoSigla;
+            Curso = curso;
+            Email = email;
+        }
+    }
+}
---
+++ E-Conc/E-Conc/Models/ViewModels/ResumoViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace E_Conc.Models.ViewModels
+{
+    public class ResumoViewModel
+    {
+        public Produto Produto { get; private set; }
+        public ItemPedido ItemPedido { get; private set; }
+        public Aluno Aluno { get; private set; }
+        public Curso Curso { get; private set; }
+
+        public ResumoViewModel(Produto produto, ItemPedido itemPedido, Aluno aluno, Curso curso)
+        {
+            Produto = produto;
+            ItemPedido = itemPedido;
+            Aluno = aluno;
+            Curso = curso;
+        }
+    }
+}
