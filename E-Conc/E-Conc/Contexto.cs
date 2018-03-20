2d64885ff79a6896e65de128c3d929cb9048785d
Marco Antonio Moyses Monteiro Junior <marco.monteiro@fatec.sp.gov.br>
2018-03-20 11:41:53 -0300

    EFCore C_DataBase E-Conc Migration 'Inicial'

--- E-Conc/E-Conc/Contexto.cs
+++ E-Conc/E-Conc/Contexto.cs
@@ -6,6 +6,7 @@ namespace E_Conc
     public class Contexto : DbContext
     {
         public DbSet<Produto> Produtos { get; set; }
+        public DbSet<ItemPedido> ItensPedido { get; set; }
 
         public Contexto(DbContextOptions<Contexto> options) : base(options)
         {
---
+++ E-Conc/E-Conc/Migrations/20180320134835_Inicial.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace E_Conc.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Curso",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    Sigla = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Curso", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orientador",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orientador", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Arquivo = table.Column<string>(nullable: true),
+                    Categoria = table.Column<int>(nullable: false),
+                    Nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItensPedido",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CursoId = table.Column<int>(nullable: true),
+                    OrientadorId = table.Column<int>(nullable: true),
+                    ProdutoId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItensPedido", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ItensPedido_Curso_CursoId",
+                        column: x => x.CursoId,
+                        principalTable: "Curso",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ItensPedido_Orientador_OrientadorId",
+                        column: x => x.OrientadorId,
+                        principalTable: "Orientador",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ItensPedido_Produtos_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "Produtos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItensPedido_CursoId",
+                table: "ItensPedido",
+                column: "CursoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItensPedido_OrientadorId",
+                table: "ItensPedido",
+                column: "OrientadorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItensPedido_ProdutoId",
+                table: "ItensPedido",
+                column: "ProdutoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItensPedido");
+
+            migrationBuilder.DropTable(
+                name: "Curso");
+
+            migrationBuilder.DropTable(
+                name: "Orientador");
+
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
