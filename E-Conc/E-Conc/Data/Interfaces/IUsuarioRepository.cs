1c3bfabaa4b6a2908c8cf653f5b42fe15bc02664
Marco Antonio Moyses Monteiro Junior <marco.monteiro@fatec.sp.gov.br>
2018-04-30 21:31:12 -0300

    Fluxo de Login Implementado e Funcional | TODO Anterior Postergado

--- E-Conc/E-Conc/Data/Interfaces/IUsuarioRepository.cs
+++ E-Conc/E-Conc/Data/Interfaces/IUsuarioRepository.cs
@@ -1,9 +1,11 @@
 ﻿using E_Conc.Data.Repository.Interfaces;
 using E_Conc.Models;
+using E_Conc.Models.ViewModels;
 
 namespace E_Conc.Data.Interfaces
 {
     public interface IUsuarioRepository : IRepository<Usuario>
     {
+        Usuario GetUsuarioPorEmail(LoginViewModel login);
     }
 }
