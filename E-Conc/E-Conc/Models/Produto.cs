c9db0451e52e97c300485e4cecbfaaf2e75460d6
Marco Antonio Moyses Monteiro Junior <marco.monteiro@fatec.sp.gov.br>
2018-05-05 19:11:11 -0300

    Remoção de Fotos de Teste | View de Detalhes com Lacunas de Model | Adicionado ao Modelo de Produto Conhecimentos

--- E-Conc/E-Conc/Models/Produto.cs
+++ E-Conc/E-Conc/Models/Produto.cs
@@ -4,30 +4,32 @@ namespace E_Conc.Models
 {
     public class Produto : BaseModel
     {
-        public string Nome { get; set; }
-        public string Arquivo { get; set; }
-        public bool Disponivel { get; set; }
+        public string Nome { get; private set; }
+        public string Arquivo { get; private set; }
+        public bool Disponivel { get; private set; }
         public string Descricao { get; private set; }
-        public Categoria Categoria { get; set; }
-        public Usuario Usuario { get; set; }
+        public string Conhecimentos { get; private set; }
+        public Categoria Categoria { get; private set; }
+        public Usuario Usuario { get; private set; }
         public Curso Curso { get; private set; }
 
         public Produto() { }
 
         public Produto
-            (int id, string nome, string arquivo, bool disponivel, string descricao, Categoria categoria, Usuario usuario, Curso curso) 
-            : this(nome, arquivo, disponivel, descricao, categoria, usuario, curso)
+            (int id, string nome, string arquivo, bool disponivel, string descricao, string conhecimentos, Categoria categoria, Usuario usuario, Curso curso) 
+            : this(nome, arquivo, disponivel, descricao, conhecimentos, categoria, usuario, curso)
         {
             Id = id;            
         }
 
         public Produto
-            (string nome, string arquivo, bool disponivel, string descricao, Categoria categoria, Usuario usuario, Curso curso)
+            (string nome, string arquivo, bool disponivel, string descricao, string conhecimentos, Categoria categoria, Usuario usuario, Curso curso)
         {
             Nome = nome;
             Arquivo = arquivo;
             Disponivel = disponivel;
             Descricao = descricao;
+            Conhecimentos = conhecimentos;
             Categoria = categoria;
             Usuario = usuario;
             Curso = curso;
