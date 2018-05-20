8e69908b53f1c06a5e822504072360128266cb34
Marco Antonio Moyses Monteiro Junior <marco.monteiro@fatec.sp.gov.br>
2018-05-20 01:01:15 -0300

    Criação de Controller e View Index para Administradores | Configuração de Roles no StartUp.cs

---
+++ E-Conc/E-Conc/Controllers/AdministracaoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Conc.Controllers
+{
+    [Authorize(Roles = "Administrador")]
+    public class AdministracaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
