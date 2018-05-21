f460eea7527cd247dfc398a818a2229fa1b73770
Marco Antonio Moyses Monteiro Junior <marco.monteiro@fatec.sp.gov.br>
2018-05-21 18:00:43 -0300

    Inserção de Configuração de Usuário Administrador em appsettings.json | Configuração de Rolers em StartUp.cs | Criação de folder para Roles | Criação e Implementação de Classe Seed para criação de Roles | Atualização da classe Program.cs para disparar evento de criação de Roles | Teste Realizado com Sucesso, roles criadas e não replicadas.

--- E-Conc/E-Conc/Program.cs
+++ E-Conc/E-Conc/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using E_Conc.Roles;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace E_Conc
@@ -14,7 +16,24 @@ namespace E_Conc
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var serviceProvider = services.GetRequiredService<IServiceProvider>();
+                    var configuration = services.GetRequiredService<IConfiguration>();
+                    Seed.CreateRoles(serviceProvider, configuration).Wait();
+                }
+                catch (Exception exception)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(exception, "Ocorreu um erro ao criar as Regras de Usuário");
+                }
+            }
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
---
+++ E-Conc/E-Conc/Roles/Seed.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using E_Conc.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace E_Conc.Roles
+{
+    public static class Seed
+    {
+        public static async Task CreateRoles(IServiceProvider serviceProvider, IConfiguration Configuration)
+        {
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<Usuario>>();
+            string[] roleNames = { "Admin", "Orientador", "Aluno" };
+            IdentityResult roleResult;
+
+            foreach (var roleName in roleNames)
+            {
+                var roleExist = await RoleManager.RoleExistsAsync(roleName);
+                if (!roleExist)                
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));                
+            }
+        }
+    }
+}
