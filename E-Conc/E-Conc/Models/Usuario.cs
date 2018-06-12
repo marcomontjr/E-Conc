1e2cb4596eee6ca6433e057554b31c54b4a8f572
Marco Antonio Moyses Monteiro Junior <marco.monteiro@fatec.sp.gov.br>
2018-06-11 22:08:47 -0300

    Inserção de JQuery Validation

--- E-Conc/E-Conc/Models/Usuario.cs
+++ E-Conc/E-Conc/Models/Usuario.cs
@@ -1,11 +1,14 @@
 ﻿using E_Conc.Models.ViewModels;
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace E_Conc.Models
 {
     public class Usuario : IdentityUser
     {
         public string Ra { get; set; }
+        [StringLength(50, MinimumLength = 5, 
+            ErrorMessage = "Nome deve ter entre 5 e 50 caracteres")]
         public string NomeCompleto { get; set; }
         public string Instituicao { get; set; }
         public string InstituicaoSigla { get; set; }
